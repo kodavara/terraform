2a9a523819285522bdfb0be8e921b1295c03b28a
kodavara <137081810+kodavara@users.noreply.github.com>
2024-07-03 20:23:37 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "vara-terraform-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -39,7 +39,7 @@ resource "aws_elb" "bar" {
 
 /*resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-01376101673c89611"
   instance_type = "t2.micro"
   tags = {
     Name = "instance-${each.key}"
